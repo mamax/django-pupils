bec51804e5ff2f26914d6e397305b2e8aeb1a67c
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2015-06-12 09:49:49 +0300

    dynamic view of pupils and groups

--- pupils/views.py
+++ pupils/views.py
@@ -1,8 +1,21 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def pupils_list(request):
-    return render(request, 'pupils/index.html', {})
+    pupils = (
+                {'id': 1,
+                'first_name': u'Віталій',
+                'last_name': u'Подоба',
+                'ticket': 235,
+                'image': 'img/me.jpeg'},
+                {'id': 2,
+                'first_name': u'Андрій',
+                'last_name': u'Корост',
+                'ticket': 2123,
+                'image': 'img/piv.png'},
+                )
+    return render(request, 'pupils/pupils_list.html', {'pupils': pupils})
 
 def pupils_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
@@ -14,7 +27,18 @@ def pupils_delete(request, sid):
     return HttpResponse('<h1>Delete Student %s</h1>' % sid)
 
 def groups_list(request):
-    return render(request, 'pupils/groups.html', {})
+    groups = (
+                {'id': 1,
+                'leader': u'Яченюк Олег',
+                'name': u'8-A'},
+                {'id': 2,
+               'leader': u'Подоба Віталій',
+                'name': u'5-A'},
+                {'id': 3,
+               'leader': u'Іванюк Андрій',
+                'name': u'8-Б'},
+                )
+    return render(request, 'pupils/groups_list.html', {'groups': groups})
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
